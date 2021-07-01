ce408861f12338bd9a499f28e9bdbeb4fb47b1a3
Vu Thai Hoc <vuthaihoc.ttud@gmail.com>
2021-07-01 17:41:08 +0700

    added solution

--- SamePart.php
+++ SamePart.php
@@ -3,11 +3,46 @@
 
 class SamePart
 {
-    public static function sameStart(...$strings) : string {
-        return "";
+    public static function sameStart(...$strings): string
+    {
+        foreach ($strings as $k => $string) {
+            $strings[$k] = preg_split("//u", $string);
+        }
+
+        $common = self::sameStartItems(...$strings);
+
+        return implode("", $common);
     }
-    public static function sameEnd(...$strings) : string {
-        return "";
+
+    public static function sameEnd(...$strings): string
+    {
+        foreach ($strings as $k => $string){
+            $strings[$k] = array_reverse(preg_split("//u", $string));
+        }
+
+        $common = self::sameStartItems(...$strings);
+
+        return implode("", array_reverse($common));
+    }
+
+    protected static function sameStartItems(...$strings) : array{
+        $common = array_shift($strings);
+        foreach ($strings as $string) {
+            $_common = [];
+            $length = min(count($common), count($string));
+            for ($i = 0; $i < $length; $i++) {
+                if ($common[$i] == $string[$i]) {
+                    $_common[] = $common[$i];
+                } else {
+                    break;
+                }
+            }
+            $common = $_common;
+            if (count($common) == 1) {
+                return [];
+            }
+        }
+        return $common;
     }
 
 }
